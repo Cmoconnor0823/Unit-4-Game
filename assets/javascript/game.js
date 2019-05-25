4e216532a5d5cd3dbd25534b6d00bf2a77703485
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-05-24 18:50:56 -0700

    added in variables, all values set to 0 or undefined

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//This is set undefined to capture the computer's guess
+var compGuess;
+
+var wins = 0;
+var losses = 0;
+/*create 4 different undefined 
+variables linked to the button clicks*/
+var bttnOne;
+var bttnTwo;
+var bttnThree;
+var bttnFour;
+/* create variable to hold the total value of 
+the user's button cicks*/
+var userClick;
+
+
+//var to print to HTML
+var winText = document.getElementById("win-text");
+var lossesText = document.getElementById("losses-text");
+var compChoice = document.getElementById ("comp-choice");
+
+
